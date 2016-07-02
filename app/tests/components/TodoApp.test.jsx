8f12a0a7d695489bc789058f3d447389665ee120
Matthew Smalley <msmalley2000@gmail.com>
2016-07-02 17:37:23 +0200

    add handle add to do

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -10,4 +10,13 @@ describe('TodoApp', () => {
   it('should exist', () => {
     expect(TodoApp).toExist();
   });
+
+  it('should add todo to the todos state on handle add todo', () => {
+    var todoText= 'test text';
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos: []});
+    todoApp.handleAddToDo(todoText);
+
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+  });
 });
