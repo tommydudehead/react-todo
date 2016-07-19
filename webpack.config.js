b0b96b6a8a249444f1f5c3c0150a6e65f80f24c7
Smalley <a01487@prod.norsk-tipping.no>
2016-07-19 14:46:36 +0200

    webconfig reload 2

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var path = require('path');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+
 module.exports = {
   entry: [
     'script!jquery/dist/jquery.min.js',
@@ -60,5 +61,5 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map'
 };
