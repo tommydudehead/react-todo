ec24cd64c61e0bf80b1be225a0f9564753ac894c
Smalley <a01487@prod.norsk-tipping.no>
2016-07-20 12:23:20 +0200

    my attempt to get working 4

--- app/app.jsx
+++ app/app.jsx
@@ -1,14 +1,20 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
+var {Todo} = require('Todo');
+var {Nav} = require('Nav');
+var {Game} = require('Game');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-
+var Main = require('Main');
 var TodoApp = require('TodoApp');
 var actions = require('actions');
 var store = require('configureStore').configure();
 var TodoAPI = require('TodoAPI');
 
+
+store.dispatch(actions.startGetQuestions());
 store.dispatch(actions.startAddTodos());
+
 
 // Load foundation
 $(document).foundation();
@@ -18,7 +24,14 @@ require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
   <Provider store={store}>
-    <TodoApp/>
+    <Router history={hashHistory}>
+      <Route path="/" component={Main}>
+        <Route path="nav" component={Nav}/>
+        <Route path="game" component={Game} />
+        <IndexRoute component={TodoApp}/>
+      </Route>
+    </Router>
   </Provider>,
+
   document.getElementById('app')
 );
