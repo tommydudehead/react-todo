910177411b954242b326ef7f38ca8ea3789f21e8
Smalley <a01487@prod.norsk-tipping.no>
2016-07-20 12:14:07 +0200

    my attempt to get working 2

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,11 +2,9 @@ var React = require('react');
 var uuid = require('node-uuid');
 var moment = require('moment');
 
-
 import TodoList from 'TodoList'
 import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
-
 
 var TodoApp = React.createClass({
   render: function () {
