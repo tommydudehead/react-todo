32de7ec27bca42bc9e6ecaed9f354e33cf14f78d
Smalley <a01487@prod.norsk-tipping.no>
2016-07-20 13:41:38 +0200

    my attempt to get working 7

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,5 @@
 var webpack = require('webpack');
 var path = require('path');
-var envFile = require('node-env-file');
-
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-try {
-  envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'));
-} catch (e) {
-
-}
 
 module.exports = {
   entry: [
@@ -23,20 +14,6 @@ module.exports = {
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery'
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-        API_KEY: JSON.stringify(process.env.API_KEY),
-        AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
-        DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
-        STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET)
-      }
     })
   ],
   output: {
@@ -76,5 +53,5 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map'
 };
