33139cc9798bbc54d05c1ed98feb4d2f1165ea35
Smalley <a01487@prod.norsk-tipping.no>
2016-07-20 13:12:42 +0200

    my attempt to get working 6

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 var envFile = require('node-env-file');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-try {
-  envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'));
-} catch (e) {
-
-}
 
 module.exports = {
   entry: [
